67d63cc6f99f69f75a8e0cac184dfeef7309ccee
karshann <karshan951@gmail.com>
2020-05-22 14:39:17 +0200

    test fonctionnel PersonnelDAO

--- src/test/java/org/example/PersonnelDAOTest.java
+++ src/test/java/org/example/PersonnelDAOTest.java
@@ -2,19 +2,29 @@ package org.example;
 
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.Assert.*;
 
 public class PersonnelDAOTest {
 
     @Test
     public void create() {
+        Personnel P=new Personnel.Builder("Jean","Paul").telephone("02589445272").naissance(LocalDateTime.of(1997,11,18,21,05)).fonction("PDG").build();
+        PersonnelDAO PersD =new PersonnelDAO();
+        PersD.create(P);
     }
 
     @Test
     public void find() {
+        Personnel P=new Personnel.Builder("Jean","Paul").telephone("02589445272").naissance(LocalDateTime.of(1997,11,18,21,05)).fonction("PDG").build();
+        PersonnelDAO PersD =new PersonnelDAO();
+        Personnel test =PersD.find("personnel");
     }
 
     @Test
     public void delete() {
+        PersonnelDAO PersD =new PersonnelDAO();
+        PersD.delete("personnel");
     }
 }
